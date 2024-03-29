95eb7dcafd8f48e06a358211b2480e81883658d5
Ghaya-aljab <159114408+Ghaya-aljab@users.noreply.github.com>
2024-03-29 23:26:54 +0400

    Update Employee.py

--- Employee.py
+++ Employee.py
@@ -3,12 +3,15 @@ from Person import Person
 class Employee(Person):
     ''' A class representing an employee at the museum, inheriting attributes from the Person class '''
 
+    # Constructor method for initializing an Employee object with employee-specific attributes and inheritance from Person
     def __init__(self, name, age, national_id, employee_id):
+        # inherits attributes from person class (the parent class)
         super().__init__(name, age, national_id)
         self._employee_id = employee_id
 
+    # Setter and getter functions for employee attributes
     def get_employee_id(self):
         return self._employee_id
 
-    def set_employee_id(self, employee_id):  # Add employee_id as a parameter
+    def set_employee_id(self, employee_id):
         self._employee_id = employee_id
